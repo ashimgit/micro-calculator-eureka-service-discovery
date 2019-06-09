9e3939895291fafbafcd2afb95fe5edf01616c7e
ashimgit <ashimnb87@gmail.com>
2019-06-09 14:45:47 +0530

    first commit

---
+++ src/main/java/com/calculatoreureka/microcalculatoreurekaservicediscovery/MicroCalculatorEurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.calculatoreureka.microcalculatoreurekaservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCalculatorEurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCalculatorEurekaServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculatoreureka/microcalculatoreurekaservicediscovery/MicroCalculatorEurekaServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculatoreureka.microcalculatoreurekaservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCalculatorEurekaServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
