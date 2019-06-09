55334e1a3f04133d09c9dbde3cea2e8b5a059134
ashimgit <ashimnb87@gmail.com>
2019-06-09 17:38:09 +0530

    bug fixes

--- src/main/java/com/calculatoreureka/microcalculatoreurekaservicediscovery/MicroCalculatorEurekaServiceDiscoveryApplication.java
+++ src/main/java/com/calculatoreureka/microcalculatoreurekaservicediscovery/MicroCalculatorEurekaServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.calculatoreureka.microcalculatoreurekaservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MicroCalculatorEurekaServiceDiscoveryApplication {
 
